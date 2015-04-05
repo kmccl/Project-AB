9ec37be6c967a12cbcc97d9063faa046c2017720
unknown <kmccl@KTL30183324.sphsc2.washington.edu>
2015-04-04 18:21:24 -0700

    Trying out the padding for the NH folks to see how that changes the amplitude of the digital signal.

--- code/AB_play_sound_TDT.m
+++ code/AB_play_sound_TDT.m
@@ -7,8 +7,8 @@ trigdur=0.02; % in seconds
 trigdelay=1.24 + 63; % Delay trigger by 1.24 ms.  (in msecs)  p
 code = 10; 
 
-% isi = [0 0];
-isi = [2 2.25]; % in seconds--this specifies the jitter window
+isi = [0 0];
+%isi = [2 2.25]; % in seconds--this specifies the jitter window
 % [2 2.25] 
 
 %% LOAD STIMULI
