2f70d466e4cf2d5791d01745d13efc99dc43e962
unknown <kmccl@KTL30183324.sphsc2.washington.edu>
2015-04-15 19:13:55 -0700

    Working on scaling vs. attenuating. Use TARGET_FILTERED as the max (basically the unfiltered sound, but run through the filter =1) Use PerfectAudio_filtered_0pad for calibration. HL=0 across all frequencies

--- code/Scale_write_to_wav.m
+++ code/Scale_write_to_wav.m
@@ -1,14 +1,38 @@
 %This is an unnecessary program that reads in the stimulus I want, scales
 %it, and writes it to a file KM 3/18/15
+%modified to mess with the new filtered files and figuring out the scaling
+%and attenuator settings. KM 4/15/15
 
-[ba, fs] = audioread('MMBF15.wav');
-ba_scaled = ba * 0.05;
+clear all
+close all
+
+[FileName,PathName]=uigetfile('*.wav','Select the Subject .wav file for calibration')
+[data, fs] = audioread([PathName FileName])
+
+data_scaled = data.*80;
+
+max(abs(data_scaled))
 
 figure
 hold on
-plot([ba ba_scaled])
+plot([data data_scaled])
 legend('Original Sound', 'Scaled Sound')
 
 [filename, pathname] = uiputfile('*.wav', 'Choose a file name'); 
-outname = fullfile(pathname, filename);
-audiowrite(outname, ba_scaled, fs, 'bitspersample', 32);
+outname = fullfile(pathname, filename); 
+audiowrite(outname, data_scaled, 10000,'bitspersample', 32);
+
+
+% [PerfAudioFilt, fs] = audioread('PerfectAudio_filtered_0pad.wav');
+% PerfAudioFilt_scaled = PerfAudioFilt.*;
+% 
+% max(abs(PerfAudioFilt_scaled))
+% 
+% figure
+% hold on
+% plot([PerfAudioFilt PerfAudioFilt_scaled])
+% legend('Original Sound', 'Scaled Sound')
+% 
+% [filename, pathname] = uiputfile('*.wav', 'Choose a file name'); 
+% outname = fullfile(pathname, filename);
+% audiowrite(outname, PerfAudioFilt_scaled, fs, 'bitspersample', 32);
